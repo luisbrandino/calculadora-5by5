0ec66ddd8d07146ce9c2ea2c7c24137186baa440
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-18 19:31:38 -0300

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Informe o primeiro número: ");
+float a = float.Parse(Console.ReadLine());
+
+Console.Write("Informe o segundo número: ");
+float b = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a operação (+, -, *, /): ");
+char operation = Console.ReadLine().First();
+
+while (true)
+{
+    bool validOperation =
+        operation == '+' ||
+        operation == '-' ||
+        operation == '*' ||
+        operation == '/';
+
+    if (validOperation)
+        break;
+
+    Console.Write("Operação inválida, tente novamente (+, -, *, /): ");
+    operation = Console.ReadLine().First();
+}
+
+while (operation == '/' && b == 0)
+{
+    Console.Write("Segundo número não pode ser zero ao dividir, digite outro valor: ");
+    b = float.Parse(Console.ReadLine());
+}
+
+float result;
+
+if (operation == '+')
+    result = a + b;
+else if (operation == '-')
+    result = a - b;
+else if (operation == '*')
+    result = a * b;
+else 
+    result = a / b;
+
+Console.WriteLine($"({a}) {operation} ({b}) = {result}");
